9103c98a0d1e823cc9b6e5d3cf5e448f54babf63
Diane Rothrock <drothrock1@yahoo.com>
2018-04-05 11:02:27 -0400

    Initial commit

---
+++ PageThirteenViewController.h
@@ -0,0 +1,40 @@
+//
+//  PageThirteenViewController.h
+//  Boston
+//
+//  Created by Diane on 11/7/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
+
+@interface PageThirteenViewController : UIViewController <AVAudioPlayerDelegate>
+{
+    IBOutlet UIScrollView *scrollView;
+    UIColor *defaultTintColor;
+    
+    UIView *slideUpView;
+    
+    AVAudioPlayer *player;
+    UIButton *playButton;
+    UISlider *progressBar;
+    UILabel *currentTime;
+    UILabel *duration;
+    NSTimer	*updateTimer;
+    
+    BOOL stopAudioOverride;
+}
+
+@property (nonatomic, retain) AVAudioPlayer	*player;
+@property (nonatomic, retain) UIButton *playButton;
+@property (nonatomic, retain) UISlider *progressBar;
+@property (nonatomic, retain) UILabel *currentTime;
+@property (nonatomic, retain) UILabel *duration;
+
+- (IBAction)photoTapped:(id)sender;
+
+@end
+
+
+
